4f6ac5c7356a3727f8c9b0e777732934b4693d56
psvfx <psvfxinvest@gmail.com>
2023-05-17 18:12:11 +0300

    add tasks 8-10 module 5: The this keyword. Prototypes and classes.

--- js/auto_check_mod_5.js
+++ js/auto_check_mod_5.js
@@ -550,7 +550,38 @@
 
 // ЗАВДАННЯ----------------
 
+// Виконай рефакторинг класу Car таким чином, щоб він приймав один параметр - об'єкт з властивостями brand, model і price. Деструктуризуй об'єкт в сигнатурі (підписі) конструктора.
+
+// ТЕСТИ
+// Оголошений клас Car
+// Клас Car має метод constructor
+// В результаті виклику new Car({ brand: "Audi", model: "Q3", price: 36000 }) утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
+// В результаті виклику new Car({ brand: "BMW", model: "X5", price: 58900 }) утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
+// В результаті виклику new Car({ brand: "Nissan", model: "Murano", price: 31700 }) утвориться об'єкт { brand: "Nissan", model: "Murano", price: 31700 }
+
+// class Car {
+//   // Change code below this line
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
+
 // РОЗВ'ЯЗАННЯ-------------
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+
+// console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 })); // { brand: "Audi", model: "Q3", price: 36000 }
+// console.log(new Car({ brand: "BMW", model: "X5", price: 58900 })); // {brand: 'BMW', model: 'X5', price: 58900}
+// console.log(new Car({ brand: "Nissan", model: "Murano", price: 31700 })); // {brand: 'Nissan', model: 'Murano', price: 31700}
 
 // ==========================================================
 //             ==== ЗАВДАННЯ 9/20 ====
@@ -558,29 +589,191 @@
 
 // ТЕОРІЯ------------------
 
+// Для роботи з властивостями майбутнього екземпляра використовуються методи класу. Методи - це просто функції, але з однією відмінністю - вони доступні екземпляру класу.
+
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+
+//   // Метод getEmail
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   // Метод changeEmail
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
 // ЗАВДАННЯ----------------
 
+// Додай класу Car два методи.
+
+// ТЕСТИ
+// getPrice() - повертає значення властивості price з об'єкта, який буде його викликати.
+// changePrice(newPrice) - оновлює значення властивості price в об'єкта, який буде його викликати на newPrice.
+// В класі Car оголошений метод getPrice
+// Метод getPrice повертає значення властивості price екземпляра класу, який його викликає
+// В класі Car оголошений метод changePrice
+// Метод changePrice змінює значення властивості price екземпляра класу, який його викликає
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+
+//   // Change code above this line
+// }
+
 // РОЗВ'ЯЗАННЯ-------------
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+//   getPrice() {
+//     return this.price;
+//   }
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+//   // Change code above this line
+// }
 
 // ==========================================================
 //             ==== ЗАВДАННЯ 10/20 ====
 // ==========================================================
 
-// ТЕОРІЯ------------------
+// ЗАДАЧА: СКЛАД;
 
 // ЗАВДАННЯ----------------
 
+// Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. Клас очікує тільки один аргумент - початковий масив товарів, який записується у властивість items об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getItems() - повертає масив поточних товарів у властивості items об'єкта, який викликає цей метод.
+// addItem(newItem) - приймає новий товар newItem і додає його в масив товарів у властивості items об'єкта, який викликає цей метод.
+// removeItem(itemToRemove) - приймає товар itemToRemove і видаляє його з масиву товарів у властивості items об'єкта, який викликає цей метод.
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
+
+// ТЕСТИ
+// Оголошений клас Storage
+// В класі Storage оголошений метод getItems
+// В класі Storage оголошений метод addItem
+// В класі Storage оголошений метод removeItem
+// Метод getItems повертає значення властивості items екземпляра класу, який його викликає
+// Метод addItem змінює властивість items екземпляра класу, який його викликає
+// Метод removeItem змінює властивість items екземпляра класу, який його викликає
+// В результаті виклику new Storage(["Nanitoids", "Prolonger", "Antigravitator"]) значення змінної storage - це об'єкт
+// Об'єкт storage містить властивість items
+// Перший виклик storage.getItems(), відразу після інціалізаціі екземпляра, повертає масив ["Nanitoids", "Prolonger", "Antigravitator"]
+// Другий виклик, storage.getItems(), після виклику storage.addItem("Droid"), повертає масив ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// Третій виклик storage.getItems(), після виклику storage.removeItem("Prolonger"), повертає масив ["Nanitoids", "Antigravitator", "Droid"]
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
 // РОЗВ'ЯЗАННЯ-------------
+
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+
+//   getItems() {
+//     return this.items;
+//   }
+
+//   addItem(newItem) {
+//     this.items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     const item = this.items.indexOf(itemToRemove);
+//     console.log(item);
+//     this.items.splice(item, 1);
+//     return this.items;
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 // ==========================================================
 //             ==== ЗАВДАННЯ 11/20 ====
 // ==========================================================
 
-// ТЕОРІЯ------------------
+// ЗАДАЧА: КОНСТРУКТОР РЯДКІВ
 
 // ЗАВДАННЯ----------------
 
+// Напиши клас StringBuilder, який приймає один параметр initialValue - довільний рядок, який записується у властивість value об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getValue() - повертає поточне значення властивості value.
+// padEnd(str) - отримує параметр str (рядок) і додає його в кінець значення властивості value об'єкта, який викликає цей метод.
+// padStart(str) - отримує параметр str (рядок) і додає його на початок значення властивості value об'єкта, який викликає цей метод.
+// padBoth(str) - отримує параметр str (рядок) і додає його на початок і в кінець значення властивості value об'єкта, який викликає цей метод.
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
+
+// Оголошений клас StringBuilder
+// В класі StringBuilder оголошений метод getValue
+// Метод getValue повертає значення властивості value екземпляра класу, який його викликає
+// В класі StringBuilder оголошений метод padEnd
+// Метод padEnd змінює властивість value екземпляра класу, який його викликає
+// В класі StringBuilder оголошений метод padStart
+// Метод padStart змінює властивість value екземпляра класу, який його викликає
+// В класі StringBuilder оголошений метод padBoth
+// Метод padBoth змінює властивість value екземпляра класу, який його викликає
+// В результаті виклику new StringBuilder(".") значення змінної builder - це об'єкт
+// Об'єкт builder містить властивість value
+// Перший виклик builder.getValue(), відразу після ініціалізації екземпляра, повертає рядок .
+// Другий виклик builder.getValue(), після виклику builder.padStart("^"), повертає рядок ^.
+// Третій виклик builder.getValue(), після виклику builder.padEnd("^"), повертає рядок ^.^
+// Четвертий виклик builder.getValue(), після виклику builder.padBoth("="), повертає рядок =^.^=
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
 // РОЗВ'ЯЗАННЯ-------------
+
+// Change code above this line
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
 
 // ==========================================================
 //             ==== ЗАВДАННЯ 12/20 ====
